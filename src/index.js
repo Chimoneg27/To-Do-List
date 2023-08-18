2357b2aad204e75c4e536639e9363e020fa92f9f
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-08-18 02:33:10 +0200

    Add to list functionality

--- src/index.js
+++ src/index.js
@@ -1,37 +1,114 @@
 import './style.css';
 
-const taskContainer = document.getElementById('tasks');
+const form = document.getElementById('todoform');
+const todoInput = document.getElementById('newtodo');
+const toDoListEl = document.getElementById('todos-list');
+let todos = JSON.parse(localStorage.getItem('todos')) || [];
+let EditTodoId = -1;
 
-const toDoArr = [
-  {
-    desc: 'I will clean my room',
-    bool: 'true',
-    index: 0,
-  },
-  {
-    desc: 'I have to read a book',
-    bool: 'true',
-    index: 1,
-  },
-  {
-    desc: 'Play fifa with Camavinga',
-    bool: 'false',
-    index: 2,
-  },
-];
+const renderTodos = () => {
+  toDoListEl.innerHTML = '';
 
-const getTasks = () => {
-  toDoArr.forEach((activity, index) => {
-    taskContainer.innerHTML += `
-      <li class="activity">
-        <div class="checkBoxex">
-          <input type="checkbox" name="action1" id="${index}"/>
-          <label for="${index}">${activity.desc}</label><br>
+  todos.forEach((todo, index) => {
+    toDoListEl.innerHTML += `
+        <div class="todo" id="${index}">
+            <i 
+              class="bi ${todo.checked ? 'bi-check-circle-fill' : 'bi-circle'}"
+              style="color : ${todo.color}"
+              data-action="check"
+              ></i>
+            <p class="" data-action="check">${todo.value}</p>
+            <i class="bi bi-pencil-square" data-action="edit"></i>
+            <i class="bi bi-trash" data-action="delete"></i>
         </div>
-        <i class="fa-solid fa-ellipsis-vertical fs-3 "></i>
-      </li>
-    `;
+        `;
   });
 };
 
-getTasks();
+renderTodos();
+
+// eslint-disable-next-line consistent-return
+const saveTodo = () => {
+  const todoValue = todoInput.value;
+
+  const isEmpty = todoValue === '';
+
+  const isDuplicate = todos.some((todo) => todo.value.toUpperCase() === todoValue.toUpperCase());
+
+  if (isEmpty) {
+    return '';
+  } if (isDuplicate) {
+    return '';
+  }
+  if (EditTodoId >= 0) {
+    todos = todos.map((todo, index) => ({
+      ...todo,
+      value: index === EditTodoId ? todoValue : todo.value,
+    }));
+    EditTodoId = -1;
+  } else {
+    todos.push({
+      value: todoValue,
+      checked: false,
+      color: 'black',
+    });
+  }
+
+  todoInput.value = '';
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  saveTodo();
+  renderTodos();
+  localStorage.setItem('todos', JSON.stringify(todos));
+});
+
+function checkTodo(todoId) {
+  todos = todos.map((todo, index) => ({
+    ...todo,
+    checked: index === todoId ? !todo.checked : todo.checked,
+  }));
+
+  renderTodos();
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function editTodo(todoId) {
+  todoInput.value = todos[todoId].value;
+  EditTodoId = todoId;
+}
+
+function deleteTodo(todoId) {
+  todos = todos.filter((todo, index) => index !== todoId);
+  EditTodoId = -1;
+  renderTodos();
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+toDoListEl.addEventListener('click', (event) => {
+  const { target } = event;
+  const parentElement = target.parentNode;
+
+  if (parentElement.className !== 'todo') return;
+
+  const todo = parentElement;
+  const todoId = Number(todo.id);
+
+  const { action } = target.dataset;
+
+  switch (action) {
+    case 'check':
+      checkTodo(todoId);
+      break;
+    case 'edit':
+      editTodo(todoId);
+      break;
+    case 'delete':
+      deleteTodo(todoId);
+      break;
+    default:
+      break;
+  }
+});
